5a0ca0b19cdf939269f26cbfdffab44885e25551
MrGlist2 <semka0222@gmail.com>
2026-01-16 19:06:48 +0700

    Добавлен пве

--- mobs/skeleton.gd
+++ mobs/skeleton.gd
@@ -3,20 +3,8 @@ extends CharacterBody2D
 var chase = false
 var speed = 10
 const jump_velocity = -100
-var jumpf = false
-func _ready() -> void:
-	var timer = Timer.new()
-	timer.wait_time = 0.7
-	timer.one_shot = false
-	timer.autostart = true
-	add_child(timer)
-	timer.timeout.connect(_on_timer_timeout)	
-	timer.start()
-	
-	
-func _on_timer_timeout():
-	if jumpf == true:
-		velocity.y = jump_velocity
+var alive = true
+
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta * 0.3
@@ -25,14 +13,31 @@ func _physics_process(delta: float) -> void:
 	if chase == true:
 		velocity.x = speed * direction.x
 	else:
-		velocity.x =0 
+		velocity.x =0
+		
+	if chase == true and is_on_floor():
+		velocity.y = jump_velocity
+	if direction.x < 0:
+		$Sprite2D.flip_h = true
+	if direction.x > 0:
+		$Sprite2D.flip_h = false
 	move_and_slide()
 
 func _on_detector_body_entered(body: Node2D) -> void:
 	if body.name == "Adlof":
 		chase = true
-		jumpf = true	
+
 func _on_detector_body_exited(body: Node2D) -> void:
 	if body.name == "Adlof":
 		chase = false
-		jumpf = false
+
+
+func _on_death_body_entered(body: Node2D) -> void:
+	if body.name == "Adlof":
+		alive = false
+		queue_free()
+		death()
+func death ():
+	if alive == false:
+		get_tree().change_scene_to_file("res://menu.tscn")
+		
