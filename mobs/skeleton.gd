43cfc22181a7d3b0a429a5ac3967d8ba1e934da4
MrGlist2 <semka0222@gmail.com>
2026-01-16 17:53:45 +0700

    Добавлен прыжок так называемому Скелетону

--- mobs/skeleton.gd
+++ mobs/skeleton.gd
@@ -1,18 +1,38 @@
 extends CharacterBody2D
 
 var chase = false
-var speed = 300
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var speed = 10
+const jump_velocity = -100
+var jumpf = false
+func _ready() -> void:
+	var timer = Timer.new()
+	timer.wait_time = 0.7
+	timer.one_shot = false
+	timer.autostart = true
+	add_child(timer)
+	timer.timeout.connect(_on_timer_timeout)	
+	timer.start()
+	
+	
+func _on_timer_timeout():
+	if jumpf == true:
+		velocity.y = jump_velocity
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * delta * 0.3
 	var player = $"../../player/Adlof"
 	var direction = (player.position - self.position).normalized()
 	if chase == true:
 		velocity.x = speed * direction.x
+	else:
+		velocity.x =0 
 	move_and_slide()
 
 func _on_detector_body_entered(body: Node2D) -> void:
 	if body.name == "Adlof":
-		chase = true	
+		chase = true
+		jumpf = true	
+func _on_detector_body_exited(body: Node2D) -> void:
+	if body.name == "Adlof":
+		chase = false
+		jumpf = false
